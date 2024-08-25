d82014ca1fbf32ac6e525168817e250d5e4532a0
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-08-24 23:08:45 -0700

    added most of the js
    I honestly just copied a medium article, but it has essentially 2 real functions, which either adds a docRef/ adds the quantity, or deletes a docRef and decreases the quantity. Update inventory then pushes that change to firebase.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,87 @@
-import Image from "next/image";
+'use client'
+
+import {useState, useEffect } from 'react';
+import {Box, Stack, Typography, Button, Modal, TextField}  from '@mui/material';
+import {firestore} from '@/firebase';
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  deleteDoc,
+  getDoc,
+} from 'firebase/firestore'
+
+const style = {
+  position:'absolute',
+  top: '50%', 
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+}
+
+
 
 export default function Home() {
+  const[inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('');
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({name: doc.id, ...doc.data() })
+    })
+    setInventory(inventoryList);
+  }
+
+  useEffect(() => {
+    updateInventory();
+  }, [])
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()){
+      const {quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity +1});
+    }
+    else{
+      await setDoc(docRef, {quantity: 1})
+    }
+    await updateInventory()
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()){
+      const {quantity } = docSnap.data();
+      if (quantity === 1){
+        await deleteDoc(docRef);
+      }
+      else{
+        await setDoc(docRef, {quantity: quantity-1})
+      }
+    }
+    await updateInventory();
+
+
+  }
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
   return (
-    <>Hello World</>
+      <Box>
+        <Typography variant="h1">Inventory Management</Typography>
+      </Box>
   );
 }
